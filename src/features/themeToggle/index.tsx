c7a9d559080ab91d0017c9e52283350dfecee7bc
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-08-07 12:47:25 +0100

    Update ThemeToggle

--- src/features/themeToggle/index.tsx
+++ src/features/themeToggle/index.tsx
@@ -2,6 +2,17 @@ import { toggleTheme } from '@/features/themeToggle/themeToggleSlice';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const StyledThemeToggle = styled.div`
+  opacity: 0.8;
+  cursor: pointer;
+
+  :hover {
+    opacity: 1;
+    transition: 250ms ease-in-out;
+  }
+`;
 
 const ThemeToggle = () => {
   const dispatch = useDispatch();
@@ -12,9 +23,9 @@ const ThemeToggle = () => {
   };
 
   return (
-    <div onClick={handleClick}>
+    <StyledThemeToggle onClick={handleClick}>
       <FontAwesomeIcon icon={!isDark ? faSun : faMoon} />
-    </div>
+    </StyledThemeToggle>
   );
 };
 
