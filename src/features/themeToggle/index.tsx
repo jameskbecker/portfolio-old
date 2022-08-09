d9621d886c89689453bf2bc1b5220d4b0671e2fc
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-08-09 16:06:41 +0100

    Improve ThemeToggle

--- src/features/themeToggle/index.tsx
+++ src/features/themeToggle/index.tsx
@@ -1,10 +1,17 @@
 import { toggleTheme } from '@/features/themeToggle/themeToggleSlice';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { faAdjust } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  motion,
+  useAnimationControls,
+  useInView,
+  Variants,
+} from 'framer-motion';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 
-const StyledThemeToggle = styled.div`
+const StyledThemeToggle = styled(motion.div)`
   opacity: 0.8;
   cursor: pointer;
 
@@ -14,17 +21,41 @@ const StyledThemeToggle = styled.div`
   }
 `;
 
+const variants: Variants = {
+  default: { transform: 'rotate(0deg)', transition: { duration: 0.25 } },
+  rotate: { transform: 'rotate(180deg)', transition: { duration: 0.25 } },
+  hover: { transform: 'rotate(45deg)', transition: { duration: 0 } },
+  hoverDark: { transform: 'rotate(225deg)', transition: { duration: 0 } },
+};
+
 const ThemeToggle = () => {
   const dispatch = useDispatch();
+  const controls = useAnimationControls();
   const { isDark } = useSelector((state: any) => state.theme);
 
+  useEffect(() => {
+    controls.set('default');
+  }, []);
+
   const handleClick = () => {
+    controls.set(!isDark ? 'hoverDark' : 'hover');
+    controls.set(!isDark ? 'rotate' : 'default');
     dispatch(toggleTheme());
   };
 
+  const handleHoverEnd = () => {
+    controls.set(isDark ? 'rotate' : 'default');
+  };
+
   return (
-    <StyledThemeToggle onClick={handleClick}>
-      <FontAwesomeIcon icon={!isDark ? faSun : faMoon} />
+    <StyledThemeToggle
+      variants={variants}
+      animate={controls}
+      whileHover={isDark ? 'hoverDark' : 'hover'}
+      onHoverEnd={handleHoverEnd}
+      onClick={handleClick}
+    >
+      <FontAwesomeIcon icon={faAdjust} />
     </StyledThemeToggle>
   );
 };
