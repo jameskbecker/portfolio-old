a0078060053762cff20122563c0fdf86511f76a7
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-09-14 09:42:16 +0100

    Update ContactSection text

--- src/components/sections/contactSection/index.tsx
+++ src/components/sections/contactSection/index.tsx
@@ -7,7 +7,7 @@ const ContactSection = () => {
   return (
     <StyledContactSection id="contact">
       <H1>Have a project in mind?</H1>
-      <H3>Let&apos;s work together!</H3>
+      <H3>I&apos;d love to hear about it!</H3>
 
       <ContactGrid />
     </StyledContactSection>
