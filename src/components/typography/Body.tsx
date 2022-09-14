413cc3b12cace66dc549fbac4f0b8195f66dd246
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-09-14 09:46:15 +0100

    Update Body.tsx

--- src/components/typography/Body.tsx
+++ src/components/typography/Body.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 export const BodySmall = styled.div`
   font-size: 1.125rem;
+  font-weight: 300;
   color: ${({ theme }: any) => theme.text};
 
   @media only screen and (${mobileMax}) {
@@ -12,6 +13,7 @@ export const BodySmall = styled.div`
 
 export const Body = styled.div`
   font-size: 1.5rem;
+  font-weight: 300;
   color: ${({ theme }: any) => theme.text};
 
   @media only screen and (${mobileMax}) {
@@ -21,6 +23,7 @@ export const Body = styled.div`
 
 export const BodyLarge = styled.div`
   font-size: 6rem;
+  font-weight: 300;
   text-transform: uppercase;
   color: ${({ theme }: any) => theme.text};
 
