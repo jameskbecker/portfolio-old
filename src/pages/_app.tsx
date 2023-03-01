8949a77f4e6ee2675ecc5341ed7ef30a6e7be802
James Becker <48602730+jameskbecker@users.noreply.github.com>
2023-03-01 18:49:40 +0100

    Setup tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import store from '@/app/store';
 import ThemeProvider from '@/features/themeToggle/ThemeProvider';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import '../globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#263793',
+        brandDark: '#5A6BC4',
+      },
+    },
+  },
+  plugins: [],
+};
