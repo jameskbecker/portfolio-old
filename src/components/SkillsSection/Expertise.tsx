aa06da485d8a5c760ce08baeae50c6294065ac95
James Becker <48602730+jameskbecker@users.noreply.github.com>
2023-03-03 17:33:28 +0100

    Update Expertise.tsx

--- src/components/SkillsSection/Expertise.tsx
+++ src/components/SkillsSection/Expertise.tsx
@@ -2,7 +2,7 @@ import { topFour } from './skillsData';
 
 const Expertise = () => {
   return (
-    <div className="flex h-full flex-1 flex-col gap-8 overflow-hidden">
+    <div className="gap-8o flex h-full flex-1 flex-col  overflow-hidden">
       <h2 className="text-5xl font-semibold uppercase text-slate-900 dark:text-slate-100 lg:text-6xl">
         My Expertise
       </h2>
