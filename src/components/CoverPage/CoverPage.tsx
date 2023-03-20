f5f921f5f891a39e06ad1d43d4feb9121057b89e
James Becker <48602730+jameskbecker@users.noreply.github.com>
2023-03-20 09:34:57 +0100

    Update CoverPage.tsx

--- src/components/CoverPage/CoverPage.tsx
+++ src/components/CoverPage/CoverPage.tsx
@@ -10,35 +10,32 @@ const CoverPage = ({ heading, subheading }: any) => {
   useEffect(() => {
     if (inView) {
       controls.start('slideIn');
-    } else {
-      controls.start('initial');
+      return;
     }
+    controls.start('initial');
   }, [inView, controls]);
 
   return (
     <div className="container mx-auto flex h-screen flex-col gap-8 bg-gray-100 px-4 dark:bg-gray-900">
-      <div className="flex flex-1 items-end">
-        <motion.h1
-          ref={ref}
-          initial="initial"
-          animate={controls}
-          variants={headingVariants}
-          className="text-5xl uppercase text-slate-900 dark:text-slate-100 lg:text-6xl"
-        >
-          {heading}
-        </motion.h1>
-      </div>
-      <div className="flex-1">
-        <motion.h3
-          ref={ref}
-          initial="initial"
-          animate={controls}
-          variants={subheadingVariants}
-          className="lg:4xl text-3xl font-light text-slate-500 dark:text-slate-500"
-        >
-          {subheading}
-        </motion.h3>
-      </div>
+      <motion.h1
+        ref={ref}
+        initial="initial"
+        animate={controls}
+        variants={headingVariants}
+        className="flex flex-1 items-end text-5xl uppercase text-slate-900 dark:text-slate-100 lg:text-6xl"
+      >
+        {heading}
+      </motion.h1>
+
+      <motion.h3
+        ref={ref}
+        initial="initial"
+        animate={controls}
+        variants={subheadingVariants}
+        className="lg:4xl flex-1 text-3xl font-light text-slate-500 dark:text-slate-500"
+      >
+        {subheading}
+      </motion.h3>
     </div>
   );
 };
