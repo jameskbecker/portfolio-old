ab4198b7ff76a3d7603c972c1132686b401dbbfe
James Becker <48602730+jameskbecker@users.noreply.github.com>
2023-03-20 09:29:55 +0100

    Update Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,10 @@
-import { motion } from 'framer-motion';
-import { ButtonProps } from './types';
+import { HTMLMotionProps, motion } from 'framer-motion';
 
-const Button = ({ label, ...rest }: ButtonProps) => (
+interface ButtonProps extends HTMLMotionProps<'button'> {
+  label?: string;
+}
+
+export const Button = ({ label, ...rest }: ButtonProps) => (
   <motion.button
     className="appearance-none rounded-lg border border-emerald-500 bg-emerald-500 py-2 px-4 opacity-90 duration-300 ease-in-out hover:opacity-100 focus:outline-amber-300 dark:border-emerald-400 dark:bg-emerald-400 dark:focus:outline-amber-400"
     draggable="false"
