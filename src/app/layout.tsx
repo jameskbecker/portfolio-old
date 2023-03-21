ec47f84988080affefce77b9b4fe2b547222baa4
James Becker <48602730+jameskbecker@users.noreply.github.com>
2023-03-21 16:24:54 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <NavigationBar logoText="James K. Becker" />
-        <main className="relative m-0 flex flex-col overflow-hidden p-0 selection:bg-emerald-500 dark:selection:bg-emerald-400">
+        <main className="relative m-0 flex flex-col overflow-hidden p-0 selection:bg-emerald-500 selection:text-white dark:selection:bg-emerald-400">
           {children}
         </main>
       </body>
